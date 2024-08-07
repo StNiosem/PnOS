c3701728b33cd7ce5ccccecd83318ec2911580f9
StNiosem <bauch.aristide@gmail.com>
2024-08-07 15:15:57 +0200

    qsd

--- lib/plugin/builtin/bootloader/bootloader.py
+++ lib/plugin/builtin/bootloader/bootloader.py
@@ -1,4 +1,5 @@
 def bl_init(command: str, diskId: int, statusLogPath: str, logFileContents: str) : 
-    # Command: {boot|list|repair}, diskId: Sata Port No. (0-999), statusLogPath: log file path (diskId/$PATH); can be empty, logFileContents: what to do if log file isn't empty {overwrite|add}  
+    # Command: {boot|list|repair}, diskId: Sata Port No. (0-999), statusLogPath: log file path (diskId/$PATH); can be empty, 
+    # logFileContents: what to do if log file isn't empty {overwrite|add}  
     print("0x000001 BOOT SUCCESS")
     print("0x000002 BL-LD PYTHON SUCCESS")
